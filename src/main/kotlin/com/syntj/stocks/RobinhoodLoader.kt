e13572a5da0b26517d618b9333053010cd7c1fb2
njsoly <njsoly@syntj.com>
2020-07-08 23:30:21 -0500

    RobinhoodLoader: new method; tweaks

--- src/main/kotlin/com/syntj/stocks/RobinhoodLoader.kt
+++ src/main/kotlin/com/syntj/stocks/RobinhoodLoader.kt
@@ -12,6 +12,8 @@ class RobinhoodLoader {
 
     private val client: HttpClient = HttpClient.newHttpClient()
 
+    /** retrieving this URL will get you information about MSFT, and it matches that of
+     * the [instrument] fields of order_history. */
     val MSFT_INSTRUMENT_URL = "https://api.robinhood.com/instruments/50810c35-d215-4866-9758-0ada4ac79ffa/"
 
     /**
@@ -32,7 +34,11 @@ class RobinhoodLoader {
     }
 
     fun getMsftInstrument(): String {
-        return getRequest(MSFT_INSTRUMENT_URL).body() as String
+        return getInstrumentStringFromUrl(MSFT_INSTRUMENT_URL)
+    }
+    
+    fun getInstrumentStringFromUrl(instrumentUrl: String) : String {
+        return getRequest(instrumentUrl).body() as String
     }
 }
 
