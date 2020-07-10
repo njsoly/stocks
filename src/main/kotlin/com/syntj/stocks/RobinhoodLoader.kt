0ff4eee0e1f85fd0cccb34a5e5ecc07291a0ed92
njsoly <njsoly@syntj.com>
2020-07-10 00:54:31 -0500

    RobinhoodLoader: comments - referenced which github repo for interacting with actual robinhood.com

--- src/main/kotlin/com/syntj/stocks/RobinhoodLoader.kt
+++ src/main/kotlin/com/syntj/stocks/RobinhoodLoader.kt
@@ -6,6 +6,12 @@ import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 import java.util.logging.Logger
 
+/**
+ * Python repo to get order history, portfolio, and positions info:
+ * https://github.com/LichAmnesia/Robinhood
+ * You can even make trades.... be careful
+ * before using the main executables, call py install setup.py.
+ */
 class RobinhoodLoader {
 
     // TODO new data class for single order
@@ -17,6 +23,8 @@ class RobinhoodLoader {
 
     /** retrieving this URL will get you information about MSFT, and it matches that of
      * the [instrument] fields of order_history. */
+
+
 
 
 }
