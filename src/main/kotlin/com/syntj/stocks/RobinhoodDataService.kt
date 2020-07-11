c0d7ebd76aee84b181d304f70e36b0dc949d2eb1
njsoly <njsoly@syntj.com>
2020-07-11 05:46:40 -0500

    RobinhoodDataService: central access to different data

--- src/main/kotlin/com/syntj/stocks/RobinhoodDataService.kt
+++ src/main/kotlin/com/syntj/stocks/RobinhoodDataService.kt
@@ -1,5 +1,6 @@
 package com.syntj.stocks
 
+import com.syntj.stocks.representations.robinhood.OrderFromCsv
 import java.util.logging.Logger
 
 /**
@@ -13,18 +14,27 @@ class RobinhoodDataService {
     // TODO new data class for single order
     // TODO new data class for order history
 
-    val robinhoodInstrumentService = RobinhoodInstrumentService()
-    val robinhoodOrderCsvService = RobinhoodOrderCsvService()
+    private val robinhoodInstrumentService = RobinhoodInstrumentService()
+    private val robinhoodOrderCsvService = RobinhoodOrderCsvService()
     private val logger: Logger = Logger.getLogger(javaClass.simpleName)
 
     /** retrieving this URL will get you information about MSFT, and it matches that of
      * the [instrument] fields of order_history. */
 
 
+    fun getMsftInstrument(): String {
+        return robinhoodInstrumentService.getMsftInstrument()
+    }
 
+    fun getOrdersFromCsv() : List<OrderFromCsv> {
+        return robinhoodOrderCsvService.loadAllOrdersFromFile()
+    }
 
 }
 
 fun main() {
-    print(RobinhoodDataService().robinhoodInstrumentService.getMsftInstrument())
+    val robinhoodDataService = RobinhoodDataService()
+    print(robinhoodDataService.getMsftInstrument())
+
+    print(robinhoodDataService.getOrdersFromCsv())
 }
