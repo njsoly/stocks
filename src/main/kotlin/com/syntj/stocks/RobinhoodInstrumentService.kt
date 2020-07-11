81bd86ac642b60f3c344ba5753a90c821a159044
njsoly <njsoly@syntj.com>
2020-07-10 21:38:12 -0500

    comments

--- src/main/kotlin/com/syntj/stocks/RobinhoodInstrumentService.kt
+++ src/main/kotlin/com/syntj/stocks/RobinhoodInstrumentService.kt
@@ -14,6 +14,10 @@ class RobinhoodInstrumentService : HttpGetter() {
         return getInstrumentStringFromUrl(MSFT_INSTRUMENT_URL)
     }
 
+    /**
+     * Get an instrument data JSON from Robinhood API.
+     * URL should be in the form https://api.robinhood.com/instruments/<UUID>
+     */
     fun getInstrumentStringFromUrl(instrumentUrl: String) : String {
         return getRequest(instrumentUrl).body() as String
     }
