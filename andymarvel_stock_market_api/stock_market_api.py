21d2b0c3c56ae02786987846f7111ab16249b259
njsoly <njsoly@syntj.com>
2020-07-10 20:49:39 -0500

    added source for andymarvel python code

--- andymarvel_stock_market_api/stock_market_api.py
+++ andymarvel_stock_market_api/stock_market_api.py
@@ -1,10 +1,12 @@
 import requests
 
-#OHLC data
+# source: https://medium.com/@andy.m9627/the-ultimate-guide-to-stock-market-apis-for-2020-1de6f55adbb
+
+# OHLC data
 #r = requests.get('https://finnhub.io/api/v1/stock/candle?token=brgrd1frh5r9t6gjc440&symbol=AAPL&resolution=1&from=1572651390&to=1572910590')
 #print(r.json())
 
-#Tick data
+# Tick data
 r = requests.get('https://finnhub.io/api/v1/stock/tick?token=brgrd1frh5r9t6gjc440&symbol=AAPL&date=2020-06-05')
 print(r.json())
 
