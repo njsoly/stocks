4923fd904cdc5e2ee78dd8c378d7a69df163b936
njsoly <njsoly@syntj.com>
2020-07-18 15:00:27 -0500

    DateUtil: new method and test class, moved testrun into test/ folder

--- src/main/kotlin/com/syntj/stocks/DateUtil.kt
+++ src/main/kotlin/com/syntj/stocks/DateUtil.kt
@@ -1,11 +1,21 @@
 package com.syntj.stocks
 
 import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 class DateUtil {
 
     companion object {
         val CENTRAL_TIME_ZONE: ZoneId = ZoneId.of("America/Chicago")
         val UTC_ZONE: ZoneId = ZoneId.of("UTC")
+
+        fun getZonedDateTimeFromInstantString(dateTimeInstantString: String): ZonedDateTime {
+            return ZonedDateTime.parse(
+                dateTimeInstantString,
+                DateTimeFormatter.ISO_INSTANT.withZone(DateUtil.CENTRAL_TIME_ZONE)
+            )
+        }
+
     }
 }
---
+++ src/test/kotlin/com/syntj/stocks/DateUtilTest.kt
@@ -0,0 +1,17 @@
+package com.syntj.stocks
+
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+class DateUtilTest {
+
+
+    private val INSTANT_STRING_UTC = "2020-06-03T15:22:00.726000Z"
+
+    @Test
+    fun `getZonedDateTimeFromInstantString with valid string does not throw error`() {
+        val result = DateUtil.getZonedDateTimeFromInstantString(INSTANT_STRING_UTC)
+        assertEquals(10, result.hour)
+    }
+}
