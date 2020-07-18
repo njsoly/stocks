dd6817b77a646bb6c1d915e1064fd467515a4f83
njsoly <njsoly@syntj.com>
2020-07-18 13:56:12 -0500

    OrderFromCsv: protected csv fields from renames with @JsonProperty annotation

--- src/main/kotlin/com/syntj/stocks/representations/robinhood/OrderFromCsv.kt
+++ src/main/kotlin/com/syntj/stocks/representations/robinhood/OrderFromCsv.kt
@@ -1,14 +1,22 @@
 package com.syntj.stocks.representations.robinhood
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 /**
  * Robinhood order history record from csv file,
  * as read in by trade_history_downloader.py.
  */
 data class OrderFromCsv (
+    @JsonProperty("side")
     val side: String,
+    @JsonProperty("symbol")
     val symbol: String,
+    @JsonProperty("shares")
     val shares: String,
+    @JsonProperty("price")
     val price: String,
+    @JsonProperty("date")
     val date: String,
+    @JsonProperty("state")
     val state: String
 )
