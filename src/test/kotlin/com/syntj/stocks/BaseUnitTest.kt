bfb6cd37e1ec051eeeb49759b0eda0717eefcda1
njsoly <njsoly@syntj.com>
2020-07-18 20:33:30 -0500

    unit tests: move a specific test up from the base class

--- src/test/kotlin/com/syntj/stocks/BaseUnitTest.kt
+++ src/test/kotlin/com/syntj/stocks/BaseUnitTest.kt
@@ -7,12 +7,8 @@ import java.nio.file.Path
 open class BaseUnitTest {
 
     companion object {
-        const val TEST_DATA_DIR = "src/test/resources"
+        private const val TEST_DATA_DIR = "src/test/resources"
         const val ORDERS_SAMPLES_PATH = "${TEST_DATA_DIR}/orders.sample.csv"
     }
 
-    @Test
-    fun `ORDERS_SAMPLES_PATH can be parsed and exists` () {
-        assertEquals(true, Path.of(ORDERS_SAMPLES_PATH).toFile().exists())
-    }
 }
--- src/test/kotlin/com/syntj/stocks/RobinhoodOrderCsvServiceTest.kt
+++ src/test/kotlin/com/syntj/stocks/RobinhoodOrderCsvServiceTest.kt
@@ -2,10 +2,16 @@ package com.syntj.stocks
 
 import org.junit.Assert.*
 import org.junit.Test
+import java.nio.file.Path
 
 class RobinhoodOrderCsvServiceTest : BaseUnitTest() {
 
     private val robinhoodOrderCsvService = RobinhoodOrderCsvService(ORDERS_SAMPLES_PATH)
+
+    @Test
+    fun `ORDERS_SAMPLES_PATH can be parsed and exists` () {
+        assertEquals(true, Path.of(ORDERS_SAMPLES_PATH).toFile().exists())
+    }
 
     @Test
     fun `RobinhoodOrderCsvServiceTest loads from sample file`() {
