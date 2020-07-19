f9bbc48662fdc21ce88a20e6492434e38fb31729
njsoly <njsoly@syntj.com>
2020-07-18 19:17:48 -0500

    ExecutedNotionalTest: unit test polish

--- src/test/kotlin/com/syntj/stocks/representations/robinhood/ExecutedNotionalTest.kt
+++ src/test/kotlin/com/syntj/stocks/representations/robinhood/ExecutedNotionalTest.kt
@@ -2,21 +2,18 @@ package com.syntj.stocks.representations.robinhood
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.syntj.stocks.BaseUnitTest
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 
-class ExecutedNotionalTest {
-    val EXAMPLE: String = "{\"amount\":\"7.26\",\"currency_code\":\"USD\",\"currency_id\":\"1072fc76-1862-41ab-82c2-485837590762\"}"
+class ExecutedNotionalTest: BaseUnitTest() {
 
-    val objectMapper = ObjectMapper().registerModule(KotlinModule())
+    private val EXAMPLE: String = "{\"amount\":\"7.26\",\"currency_code\":\"USD\",\"currency_id\":\"1072fc76-1862-41ab-82c2-485837590762\"}"
+    private val objectMapper = ObjectMapper().registerModule(KotlinModule())
 
-    @Before
-    fun setup() {
-
-    }
     @Test
-    fun deserialize() {
+    fun `deserialize-serialize results in same string`() {
         val en: ExecutedNotional = objectMapper.readValue(EXAMPLE, ExecutedNotional::class.java)
         val str: String = objectMapper.writeValueAsString(en)
 
