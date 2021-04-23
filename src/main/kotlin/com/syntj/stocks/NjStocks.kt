eb1f619b69d892980b595f5a96f3b710eb0caf31
njsoly <njsoly@syntj.com>
2021-04-23 01:25:42 -0500

    stocks: overhaul NjStocks.kt

--- src/main/kotlin/com/syntj/stocks/NjStocks.kt
+++ src/main/kotlin/com/syntj/stocks/NjStocks.kt
@@ -1,22 +1,40 @@
 package com.syntj.stocks
 
-import java.net.http.HttpResponse
-import java.util.logging.Logger
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class NjStocks {
 
-    private val logger = Logger.getLogger(javaClass.simpleName)
-
     fun run() {
-        println("************** NjStocks **************")
+        runSamples.runAll()
+    }
 
-        val quoteString: String = FinnhubRequester().requestQuoteAsString(WatchedStock.DBX)
+    companion object {
+        val logger: Logger = LoggerFactory.getLogger(this::class.java)
+        
+        object runSamples {
+            private val DEFAULT_STOCK_SYMBOL = WatchedStock.DBX
+            
+            fun requestQuoteAsString(symbol: String = DEFAULT_STOCK_SYMBOL.name) {
+                logger.info("sample: FinnhubRequester().requestQuoteAsString(\"$symbol\")")
 
-        println(quoteString)
+                val quoteString = FinnhubRequester().requestQuoteAsString(symbol)
+                logger.info("\n\tresult: $quoteString\n***\n")
 
+            }
+            
+            
+            fun runAll () {
+                this.requestQuoteAsString()
+            }
+        }
     }
 }
 
 fun main() {
+    NjStocks.logger.info("░*** <NjStocks> ***░")
+
     NjStocks().run()
+
+    NjStocks.logger.info("░*** </NjStocks> ***░")
 }
