6088477593850b73221a6ebe2c25f25a97d000f4
njsoly <njsoly@syntj.com>
2021-05-16 16:21:37 -0500

    STOX-8: Can now run integration tests

---
+++ src/integration-test/kotlin/DummyIntergrationTest.kt
@@ -0,0 +1,12 @@
+import org.junit.Test
+
+class DummyIntergrationTest {
+
+    /**
+     * Just testing out the new unbroken-dome plugin settings.
+     */
+    @Test
+    fun `wrote a test for the sake of having a test` () {
+        println("You're running an integration test.")
+    }
+}
