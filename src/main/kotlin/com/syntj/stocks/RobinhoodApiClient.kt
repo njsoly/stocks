0197b6f0c8bb67305df7ad8ed86d54b5f65a3498
njsoly <njsoly@syntj.com>
2021-05-21 22:58:04 -0500

    STOX-9: move main URLs into companion

--- src/main/kotlin/com/syntj/stocks/RobinhoodApiClient.kt
+++ src/main/kotlin/com/syntj/stocks/RobinhoodApiClient.kt
@@ -1,7 +1,12 @@
 package com.syntj.stocks
 
 class RobinhoodApiClient {
-    val quoteBaseUrl: String = "https://api.robinhood.com/quotes/"
+
+    companion object {
+        const val quoteBaseUrl: String = "https://api.robinhood.com/quotes/"
+        const val apiUrl: String = "https://api.robinhood.com"
+        const val loginUrl = "$apiUrl/oauth2/token/"
+    }
 
     // TODO get this to work.  It returns a 401.  Probably need to log in.
     fun getQuote(symbol: String) : String{
