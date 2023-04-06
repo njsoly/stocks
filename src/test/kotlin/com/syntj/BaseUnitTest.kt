b33447a252643256f7c1890bf26df6deab413898
njsoly <njsoly@syntj.com>
2023-04-06 02:14:43 -0500

    njsoly-a15-1: BaseUnitTest - ignore self

--- src/test/kotlin/com/syntj/BaseUnitTest.kt
+++ src/test/kotlin/com/syntj/BaseUnitTest.kt
@@ -2,15 +2,17 @@ package com.syntj
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import org.junit.Ignore
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 import java.nio.file.Files
 import java.nio.file.Path
 
+@Ignore
 @RunWith(JUnit4::class)
 open class BaseUnitTest {
 
-    protected val TEST_DATA_DIR = "src/test/resources"
+    private val TEST_DATA_DIR = "src/test/resources"
     val ORDERS_SAMPLES_PATH = "$TEST_DATA_DIR/orders.sample.csv"
 
     protected val objectMapper: ObjectMapper = ObjectMapper().registerModule(KotlinModule())
