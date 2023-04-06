440d4cc9ce4606b8900585a149f61fde777c52fe
njsoly <njsoly@syntj.com>
2023-04-06 02:03:50 -0500

    njsoly-a15-1: Upgrade a bunch of versions

--- src/main/kotlin/com/syntj/stocks/WatchedStock.kt
+++ src/main/kotlin/com/syntj/stocks/WatchedStock.kt
@@ -1,10 +1,21 @@
 package com.syntj.stocks
 
+/** An enumeration of some known stock symbols. */
 @Suppress("unused")
-enum class WatchedStock(longName: String) {
+enum class WatchedStock(val longName: String) {
 
     DBX("Dropbox"),
     GNUS("Genius Brands"),
     MSFT("Microsoft"),
-    MMM("3M")
+    MMM("3M");
+
+    /** Safe alternative to [valueOf].  Returns `null` if no enum entries match [longName]. */
+    fun getByLongName(longName: String) : WatchedStock? {
+        WatchedStock.values().toList().forEach {
+            if (it.longName.equals(longName, ignoreCase = true)) {
+                return it
+            }
+        }
+        return null
+    }
 }
